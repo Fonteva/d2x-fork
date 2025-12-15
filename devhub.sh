a0896907ad32a7ad43455e7731c966aca0d33c64
Dave Stelts <55215298+dstelts@users.noreply.github.com>
2025-12-15 14:49:07 -0500

    - using latest SF CLI commands in devhub.sh script - added support for custom org aliases - renamed TARGET_ORG_AUTH_URL to DEV_ORG_AUTH_URL

--- devhub.sh
+++ devhub.sh
@@ -4,91 +4,75 @@ if [ -f ~/.dev_hub_authenticated ]; then
     exit 0
 fi
 
+if [ -z "$DEV_HUB_ALIAS" ]; then
+  DEV_HUB_ALIAS="devhub"
+fi
 
 if [ -z "$DEV_HUB_AUTH_URL" ]; then
     if [ -z "$DEV_HUB_USERNAME" ]; then
-        echo "DEV_HUB_USERNAME is not set, length is $(echo $(($(echo $DEV_HUB_USERNAME|wc -c)-1))). You must set either DEV_HUB_AUTH_URL or DEV_HUB_USERNAME, DEV_HUB_CLIENT_ID, and DEV_HUB_PRIVATE_KEY."
-        exit 1
-    fi
-    if [ -z "$DEV_HUB_CLIENT_ID" ]; then
-        echo "DEV_HUB_CLIENT_ID is not set, length is $(echo $(($(echo $DEV_HUB_CLIENT_ID|wc -c)-1))). You must set either DEV_HUB_AUTH_URL or DEV_HUB_USERNAME, DEV_HUB_CLIENT_ID, and DEV_HUB_PRIVATE_KEY."
-        exit 1
-    fi
-    if [ -z "$DEV_HUB_PRIVATE_KEY" ]; then
-        echo "DEV_HUB_PRIVATE_KEY is not set, length is $(echo $(($(echo $DEV_HUB_PRIVATE_KEY|wc -c)-1))). You must set either DEV_HUB_AUTH_URL or DEV_HUB_USERNAME, DEV_HUB_CLIENT_ID, and DEV_HUB_PRIVATE_KEY."
+        echo "DEV_HUB_USERNAME is not set. You must set either DEV_HUB_AUTH_URL or DEV_HUB_USERNAME, DEV_HUB_CLIENT_ID, and DEV_HUB_PRIVATE_KEY."
         exit 1
     fi
 
-    echo "Authenticating to DevHub using JWT..."
+    if [ -z "$DEV_HUB_CLIENT_ID" ]; then
+        echo "DEV_HUB_CLIENT_ID is not set. You must set either DEV_HUB_AUTH_URL or DEV_HUB_USERNAME, DEV_HUB_CLIENT_ID, and DEV_HUB_PRIVATE_KEY."
+        exit 1
+    fi
+
+    if [ -z "$DEV_HUB_PRIVATE_KEY" ]; then
+        echo "DEV_HUB_PRIVATE_KEY is not set. You must set either DEV_HUB_AUTH_URL or DEV_HUB_USERNAME, DEV_HUB_CLIENT_ID, and DEV_HUB_PRIVATE_KEY."
+        exit 1
+    fi
+
+    echo "Authenticating to DevHub using JWT (alias $DEV_HUB_ALIAS)..."
 
     # Write the DEV_HUB_PRIVATE_KEY to a file
     echo $DEV_HUB_PRIVATE_KEY > /tmp/dev_hub.key
 
-    # Authenticate the DevHub
+    # Authenticate to the DevHub
     sf org login jwt \
         --username $DEV_HUB_USERNAME \
         --jwt-key-file /tmp/dev_hub.key \
         --client-id $DEV_HUB_CLIENT_ID \
-        --alias DevHub \
+        --alias $DEV_HUB_ALIAS \
         --set-default-dev-hub
 
     [[ -f /tmp/dev_hub.key ]] && rm /tmp/dev_hub.key
 
 else
-    # Authenticate using Auth URL
-    echo "Authenticating to DevHub using auth url..."
-
-    # Write the DEV_HUB_AUTH_URL to a file
-    echo $DEV_HUB_AUTH_URL > /tmp/dev_hub_auth_url
-
-    # Authenticate the DevHub
-    sfdx org login sfdx-url -f /tmp/dev_hub_auth_url -a DevHub -d
-
-    [[ -f /tmp/dev_hub_auth_url ]] && rm /tmp/dev_hub_auth_url
+    # Authenticate using sfdx-url
+    echo "Authenticating to DevHub using sfdx-url (alias $DEV_HUB_ALIAS)..."
+    echo $DEV_HUB_AUTH_URL | sf org login sfdx-url --sfdx-url-stdin --alias $DEV_HUB_ALIAS --set-default-dev-hub
 fi
 
 if [ -z "$PACKAGING_ORG_AUTH_URL" ]; then
-    echo "No PACKAGING_ORG_AUTH_URL set, skipping packaging org authentication."
+    echo "PACKAGING_ORG_AUTH_URL not set, skipping packaging org authentication."
 else
-    # Authenticate using Auth URL
-    echo "Authenticating to Packaging Org using auth url..."
+    if [ -z "$PACKAGING_ORG_ALIAS" ]; then
+        PACKAGING_ORG_ALIAS="packaging"
+    fi
 
-    # Write the PACKAGING_ORG_AUTH_URL to a file
-    echo $PACKAGING_ORG_AUTH_URL > /tmp/packaging_org_auth_url
-
-    # Authenticate the DevHub
-    sfdx org login sfdx-url -f /tmp/packaging_org_auth_url -a packaging
+    # Authenticate using sfdx-url
+    echo "Authenticating to packaging org using sfdx-url (alias $PACKAGING_ORG_ALIAS)..."
+    echo $PACKAGING_ORG_AUTH_URL | sf org login sfdx-url --sfdx-url-stdin --alias $PACKAGING_ORG_ALIAS
 
     # Import the org to CumulusCI
-    cci org import packaging packaging
-
-    [[ -f /tmp/packaging_org_auth_url ]] && rm /tmp/packaging_org_auth_url
+    cci org import $PACKAGING_ORG_ALIAS $PACKAGING_ORG_ALIAS
 fi
 
-if [ -z "$TARGET_ORG_AUTH_URL" ]; then
-    echo "No TARGET_ORG_AUTH_URL set, skipping target org authentication."
+if [ -z "$DEV_ORG_AUTH_URL" ]; then
+    echo "DEV_ORG_AUTH_URL not set, skipping dev org authentication."
 else
-    # Authenticate using Auth URL
-    echo "Authenticating to Target Org using auth url..."
+    if [ -z "$DEV_ORG_ALIAS" ]; then
+        DEV_ORG_ALIAS="dev"
+    fi
 
-    # Write the TARGET_ORG_AUTH_URL to a file
-    echo $TARGET_ORG_AUTH_URL > /tmp/target_org_auth_url
-
-    # Authenticate the DevHub
-    sfdx org login sfdx-url -f /tmp/target_org_auth_url -a target
+    # Authenticate using sfdx-url
+    echo "Authenticating to dev org using sfdx-url (alias $DEV_ORG_ALIAS)..."
+    echo $DEV_ORG_AUTH_URL | sf org login sfdx-url --sfdx-url-stdin --alias $DEV_ORG_ALIAS
 
     # Import the org to CumulusCI
-    cci org import target target
-
-    [[ -f /tmp/target_org_auth_url ]] && rm /tmp/target_org_auth_url
+    cci org import $DEV_ORG_ALIAS $DEV_ORG_ALIAS
 fi
-
-if [ "`whoami`" == "d2x" ]; then
-    echo "Setting dev as the default org"
-    cci org default dev
-fi
-
-# Ensure the force-app/main/default folder exists
-mkdir -p force-app/main/default
 
 touch ~/.dev_hub_authenticated
